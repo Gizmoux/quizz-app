2470f34d50c2bf9669eefa61efab23053a43d688
Gizmoux <nicolas.deriemacker@hotmail.fr>
2024-08-21 22:29:36 +0200

    add Card

---
+++ app/components/card.tsx
@@ -0,0 +1,44 @@
+"use client";
+import React from "react";
+import data from "../data.json";
+import { useState } from "react";
+
+const Card = () => {
+  const [indexQuestion, setIndexQuestion] = useState(0);
+  const [isTrue, setIsTrue] = useState(false);
+  const [counter, setCounter] = useState(0);
+  const handleClick = (option: any) => {
+    if (option === data[indexQuestion].answer) {
+      setIsTrue(true);
+      setCounter(counter + 1);
+      setIndexQuestion(indexQuestion + 1);
+    } else {
+      console.log("mauvaise réponse");
+    }
+  };
+
+  return (
+    <div>
+      <h1> Question {indexQuestion + 1} </h1>
+      <p>Score: {counter}</p>
+
+      <p>{data[indexQuestion].question}</p>
+
+      <p>
+        {data[indexQuestion].options.map((option) => {
+          return (
+            <button
+              onClick={() => handleClick(option)}
+              key={option}
+              className="bg-red-400 hover:bg-red-500 rounded-md m-2 p-2 w-1/2 flex items-center justify-center"
+            >
+              {option}
+            </button>
+          );
+        })}
+      </p>
+    </div>
+  );
+};
+
+export default Card;
