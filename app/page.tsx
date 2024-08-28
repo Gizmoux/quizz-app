b3fd952c662df861b9ca0aaca45d338e7423ce85
Gizmoux <nicolas.deriemacker@hotmail.fr>
2024-08-28 21:09:05 +0200

    update app

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Card from "./components/card";
 export default function Home() {
   return (
     <div className="flex flex-col justify-center place-items-center gap-4">
-      <h1>Hello Quizz App</h1>
+      <h1>Hello Quizz Appa</h1>
       <Card />
     </div>
   );
