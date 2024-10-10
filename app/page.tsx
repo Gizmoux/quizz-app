df45c3a1a7fdea362fa063f94de563adb5114964
Nicolas <nicolas.deriemacker@hotmail.fr>
2024-10-10 17:56:48 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Card from "./components/card";
+import Card from './components/card';
 
 export default function Home() {
-  return (
-    <div className="flex flex-col justify-center place-items-center gap-4">
-      <h1>Hello Quizz Application!!!!!</h1>
-      <Card />
-    </div>
-  );
+	return (
+		<div className="flex flex-col justify-center place-items-center gap-4">
+			<h1>Hello Quizz Application!!!!!!!</h1>
+			<Card />
+		</div>
+	);
 }
